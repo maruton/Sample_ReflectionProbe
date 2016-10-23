856aff020a2f7d90ddbbea7bc366b2c4b951c74e
maruton <marutongit@gmail.com>
2016-10-23 10:34:25 +0900

    First commit.

---
+++ Sample_ReflectionProbe/Assets/Script/Move7.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move7 : MonoBehaviour {
+
+	Vector3 pos;
+	void Start() {
+		pos = gameObject.transform.position;
+
+	}
+
+	float moveDelata = 0.1f;
+	void Update() {
+		if (pos.x >  7f) moveDelata = -0.1f;
+		if (pos.x < -7f) moveDelata =  0.1f;
+		pos.x += moveDelata;
+		gameObject.transform.position = pos;
+	}
+
+}
---
+++ Sample_ReflectionProbe/Assets/Script/Turn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turn : MonoBehaviour {
+
+	Vector3 deg;
+	void Start () {
+		deg = gameObject.transform.eulerAngles;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		deg.y += 0.2f;
+		if (deg.y > 360f) deg.y = 0f;
+		gameObject.transform.rotation = Quaternion.Euler(deg);
+	}
+}
